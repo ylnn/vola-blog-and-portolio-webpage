49c3d5c0c58f476a7c8020d67e3c5d5ced57d47b
yalin <yalincobanoglu@yandex.com>
2018-06-28 15:08:20 +0300

    publish check

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -9,12 +9,16 @@ class FrontController extends Controller
 {
     public function index()
     {
-        $posts = Post::where('type', 'POST')->latest()->get();
+        $posts = Post::where('type', 'POST')->where('status', 'published')->latest()->get();
         return view('front.home', compact('posts'));
     }
 
     public function post(Post $post)
     {
+        //check published
+        if($post->status != 'PUBLISHED'){
+            abort(404);
+        }
        return view('front.post', compact('post'));
     }
 }
