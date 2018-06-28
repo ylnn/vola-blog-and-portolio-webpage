d84d6c60ae250a0535beb0c16d6a0d6a2a9d7efd
yalin <yalincobanoglu@yandex.com>
2018-06-28 14:32:33 +0300

    seed order

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(CategoriesTableSeeder::class);
-        $this->call(DataRowsTableSeeder::class);
         $this->call(DataTypesTableSeeder::class);
+        $this->call(DataRowsTableSeeder::class);
         $this->call(MenuItemsTableSeeder::class);
         $this->call(MenusTableSeeder::class);
         $this->call(PagesTableSeeder::class);
