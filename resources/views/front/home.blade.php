dd5efff489f26161e454c7c710770926ce28c35c
yalin <yalincobanoglu@yandex.com>
2018-08-03 19:31:35 +0300

    post variable name changed

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -3,32 +3,32 @@
 @section('content')
 
     @isset($posts)
-        @foreach ($posts as $post)
-            @if( ( $post->status == "PUBLISHED" ) or (auth()->check()))
+        @foreach ($posts as $p)
+            @if( ( $p->status == "PUBLISHED" ) or (auth()->check()))
             <div class="article">
                 <div class="title">
-                    <a class="article-link" href="{{ route('post.detail', [$post->id, $post->slug]) }}">
-                        {{$post->title}}
+                    <a class="article-link" href="{{ route('post.detail', [$p->id, $p->slug]) }}">
+                        {{$p->title}}
                     </a>
                 </div>
-                @if($post->status == "DRAFT")
+                @if($p->status == "DRAFT")
                     <div class="badge badge-2x badge-warning">DRAFT</div>
                 @endif
-                <div class="date">{{$post->created_at->format('l, d F Y')}}</div>
-                @isset($post->image)
+                <div class="date">{{$p->created_at->format('l, d F Y')}}</div>
+                @isset($p->image)
                     <div class="image">
-                        <a class="article-link" href="{{ route('post.detail', [$post->id, $post->slug]) }}">
-                            <img src="{{Voyager::image($post->image)}}" class="img-fluid" />
+                        <a class="article-link" href="{{ route('post.detail', [$p->id, $p->slug]) }}">
+                            <img src="{{Voyager::image($p->image)}}" class="img-fluid" />
                         </a>
                     </div>
                 @endif
                 <div class="summary">
-                    <a class="article-link" href="{{ route('post.detail', [$post->id, $post->slug]) }}">
-                        {{ $post->excerpt}}
+                    <a class="article-link" href="{{ route('post.detail', [$p->id, $p->slug]) }}">
+                        {{ $p->excerpt}}
                     </a>
                 </div>
                 <div class="read-more">
-                    <a href="{{ route('post.detail', [$post->id, $post->slug]) }}" class="btn btn-dark">Read more...</a>
+                    <a href="{{ route('post.detail', [$p->id, $p->slug]) }}" class="btn btn-dark">Read more...</a>
                 </div>
             </div>
             @endif
