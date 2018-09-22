97469e0d916a8a39e395da413ef6bec0bc4a8c5d
yalin <yalincobanoglu@yandex.com>
2018-09-22 16:26:57 +0300

    img responsive class

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="home-content pt-4">
-        <img src="{{Voyager::image(setting('site.about-me-image'))}}" alt="">
+        <img src="{{Voyager::image(setting('site.about-me-image'))}}" class="img-fluid" alt="">
         <p>
             {!! setting('site.about-me-content') ?? '' !!}
         </p>
